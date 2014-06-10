8aa7affc00c87768d1fe4fd9796434c1bbe0d290
Brian Putnam <brian@brian-putnam.com>
2014-06-10 01:15:27 -0600

    improvements to pipeline, added sqlite import

--- pipeline.py
+++ pipeline.py
@@ -13,46 +13,47 @@ def processPipeline():
 		jsontext = json_minify(jsontext)
 		pipelines = json.loads(jsontext)
 
-	session = getKaggleSession()
-
 	for pipeline in pipelines:
 		if "dest" not in pipeline:
 			pipeline["dest"] = pipeline["url"].split("/")[-1]
-		pipeline["session"] = session
 		executePipeline(pipeline)
 		print ""
 
+_session = None
 def getKaggleSession():
-	username = passwd = ""
-	if os.path.isfile("auth.json"):
-		with open("auth.json", "r") as f:
-			auth_json = json.load(f)
-			x = auth_json["kaggle.com"]
-			username = x["un"]
-			passwd = x["pw"]
-	else:
-		print "enter kaggle account:"
-		username = raw_input()
-		print "enter kaggle password:"
-		passwd = getpass.getpass()
 
-	session = requests.Session()
-	session.get("https://www.kaggle.com/account/login")
-	session.post(
-		"https://www.kaggle.com/account/login",
-		data={
-			"UserName": username,
-			"Password": passwd
-		}
-	)
-	return session
+	if _session == None:
+		username = passwd = ""
+		if os.path.isfile("auth.json"):
+			with open("auth.json", "r") as f:
+				auth_json = json.load(f)
+				x = auth_json["kaggle.com"]
+				username = x["un"]
+				passwd = x["pw"]
+		else:
+			print "enter kaggle account:"
+			username = raw_input()
+			print "enter kaggle password:"
+			passwd = getpass.getpass()
+
+		session = requests.Session()
+		session.get("https://www.kaggle.com/account/login")
+		session.post(
+			"https://www.kaggle.com/account/login",
+			data={
+				"UserName": username,
+				"Password": passwd
+			}
+		)
+		_session = session
+	return _session
 
 def executePipeline(pipeline):
 	lastfile = downloadFile(pipeline)
 	if lastfile.endswith(".zip"):
 		lastfile = unzipFile(pipeline, lastfile)
-	# if lastfile.endswith(".csv"):
-	# 	lastfile = importCsv(pipeline, lastfile)
+	if lastfile.endswith(".csv"):
+		lastfile = importCsv(pipeline, lastfile)
 
 def downloadFile(pipeline):
 	url = pipeline["url"]
@@ -65,10 +66,10 @@ def downloadFile(pipeline):
 
 		with open(filename, 'w') as outf:
 			#use redirect to log in
-			req = pipeline["session"].get(pipeline["url"])
+			req = getKaggleSession().get(pipeline["url"])
 
 			if req.status_code != 200:
-				raise "download request failed"
+				raise Exception("download request failed")
 
 			for chunk in req.iter_content(chunk_size = 512*1024):
 				if chunk:
@@ -80,16 +81,69 @@ def unzipFile(pipeline, lastfile):
 	with zipfile.ZipFile(lastfile) as zf:
 		files = zf.infolist()
 		if len(files) > 1:
-			raise "Too many files in '%s', need to update pipeline.py to handle this" % lastfile
-		unzippedFile = files[0]
+			raise Exception("Too many files in '%s', need to update pipeline.py to handle this" % lastfile)
+		unzippedFile = files[0].filename
 
-		print "unzipping '%s'..." % lastfile
-		zf.extractall(".")
+		if os.path.isfile(unzippedFile):
+			print "already unzipped '%s', skipping..." % unzippedFile
+		else:
+			print "unzipping '%s'..." % lastfile
+			zf.extractall(".")
 
 		return unzippedFile
 
+_SQLITE_DB = "data.sqlite"
+
+_parserFns = {
+	"float": float,
+	"int": int,
+	"string": lambda x:x
+}
+
 def importCsv(pipeline, lastfile):
-	pass
+	conn = sqlite3.connect(_SQLITE_DB)
+	cursor = conn.cursor()
+
+	table = pipeline["sqlite"]["table"]
+	columns = pipeline["sqlite"]["columns"]
+
+	with open(lastfile, 'r') as csvfile:
+		reader = csv.reader(csvfile)
+		headerline = reader.next()
+
+		if not listeqset(headerline, columns.keys()):
+			print table
+			print headerline
+			print columns.keys()
+			raise Exception("The headers that were found in the csv file were different from the ones found in the pipeline.json file")
+
+		cursor.execute(
+			"CREATE TABLE %s (%s)" % (
+				table,
+				", ".join(headerline)
+			)
+		)
+
+		questionMarks = ", ".join(["?" for i in range(0, len(headerline))])
+		parsers = [_parserFns[columns[columnName]] for columnName in headerline] # make sure order matches
+		csvrows = []
+		for csvline in reader:
+			for i in range(0, len(parsers)):
+				csvline[i] = parsers[i](csvline[i])
+			csvrows.append(csvline)
+			
+		cursor.executemany("INSERT INTO %s VALUES (%s)" % (table, questionMarks), csvrows)
+	conn.commit()
+	conn.close()	
+
+def listeqset(l, s):
+	if len(l) != len(s):
+		return False
+	else:
+		for i in l:
+			if i not in s:
+				return False
+		return True
 
 # https://github.com/getify/JSON.minify/blob/master/minify_json.py
 def json_minify(string, strip_space=True):
