794bcab84269ae43b0254026e48f5b8b2e802fad
Brian Putnam <brian@brian-putnam.com>
2014-07-28 20:24:14 -0600

    moved a lot of stuff to voronoi.py for testing

--- voronoi.py
+++ voronoi.py
@@ -1,14 +1,6 @@
-import numpy as np
 import pandas as pd
-from bkputils import *
-import math, time, sys, zipfile, json, random, scipy.spatial
-
-global_start = time.time()
-
-TRAIN_LIMIT = None
-TEST_LIMIT = None
-CSV_OUTPUT_FILE = "multibayes.csv"
-ZIP_OUTPUT_FILE = CSV_OUTPUT_FILE + ".zip"
+import numpy as np
+import math, random, scipy.spatial
 
 def z_score(col):
 	"""Calculate the z-scores of a column or pandas.Series"""
@@ -19,22 +11,6 @@ def z_score(col):
 def z_scores(df):
 	"""Calculate the z-scores of a dataframe on a column-by-column basis"""
 	return df.apply(z_score, 0)
-
-def calc_col_flags(df):
-	row_sums = np.zeros(df.shape[0], dtype=np.int)
-	for index, col in enumerate(feature_cols):
-		series = df[col]
-		row_sums += np.logical_not(np.isnan(series)) * (2**index)
-	return row_sums
-
-def get_flagged_cols(col_flags, available_cols = None):
-	global feature_cols
-	if available_cols == None:
-		available_cols = feature_cols
-	return [
-		col for (index, col) in enumerate(available_cols)
-		if col_flags & 2**index > 0
-	]
 
 def calc_simplex_volumes(dtri=None, simplices=None):
 	"""
@@ -131,7 +107,17 @@ class VoronoiKde(object):
 				dt.points[neighborhood_indices],
 				dt.points[[point_index]]
 			))
-			sub_dt = scipy.spatial.Delaunay(neighborhood_points)
+			try:
+				sub_dt = scipy.spatial.Delaunay(neighborhood_points)
+			except:
+				print
+				print "name", name
+				print "point_index", point_index
+				print "dt (%d, %d)" % (len(dt.points), len(dt.points[0]))
+				print
+				print neighborhood_points
+				exit()
+
 			return np.sum(calc_simplex_volumes(dtri=sub_dt))
 			# ch = scipy.spatial.ConvexHull(neighborhood_points)
 			# simplices = np.column_stack((
@@ -157,6 +143,7 @@ class VoronoiKde(object):
 		# write("%s: calculating bin densities" % name)
 		self.bin_densities = bin_counts / bin_volumes
 		self.bin_densities = self.bin_densities / np.sum(self.bin_densities * bin_volumes)
+
 		# writeDone()
 
 	def z_scores(self, dataframe, init):
@@ -177,6 +164,7 @@ class VoronoiKdeComparator(object):
 	def __init__(self, name, dataframe, target_cols):
 
 		self.target_cols = target_cols
+		self.name = name
 
 		is_s = dataframe["Label"] == "s"
 		dataframe_s = dataframe[is_s]
@@ -190,102 +178,77 @@ class VoronoiKdeComparator(object):
 	def classify(self, dataframe):
 		df = dataframe[self.target_cols]
 		score_b = self.kde_b.score(df)
+		print "score_b", score_b
 		score_s = self.kde_s.score(df)
-		lookup = np.array(["b", "s"])
+		print "score_s", score_s
+		
 		score_ratio = score_s/score_b
-		return (
-			lookup[(score_ratio > 1.0).astype(np.int32)],
-			score_ratio
-		)
-
-def ams(predictions, actual_df):
-	predicted_signal = predictions == "s"
-	actual_signal = actual_df["Label"] == "s"
-	actual_background = ~actual_signal
-
-	s = true_positives = np.sum(actual_df[predicted_signal & actual_signal]["Weight"])
-	b = false_positives = np.sum(actual_df[predicted_signal & actual_background]["Weight"])
-	B_R = 10.0
-
-	radicand = 2.0 * ((s+b+B_R) * math.log(1.0 + (s/(b+B_R))) - s)
-
-	if radicand < 0.0:
-		print "radicand is less than 0, exiting"
+		print "score_ratio", score_ratio
 		exit()
+		return score_ratio
+
+def choose(n, k):
+	if k == 1:
+		for item in n:
+			yield [item]
+	elif len(n) == k:
+		yield n
 	else:
-		return math.sqrt(radicand)
+		current = [n[0]]
+		remaining = n[1:]
+		for sub_result in choose(remaining, k-1):
+			yield current + sub_result
+		for sub_result in choose(remaining, k):
+			yield sub_result
 
-print "TRAIN_LIMIT:", TRAIN_LIMIT
-print "TEST_LIMIT:", TEST_LIMIT
-print
+class VoronoiKdeComparatorSet(object):
+	def __init__(self, col_flags_str, dataframe, cols):
+		self.available_cols =  cols
 
-write("loading training data")
-traindata = loadTrainingData(TRAIN_LIMIT)
-traindata = traindata[:TRAIN_LIMIT]
-feature_cols = featureCols(only_float64=True)
-target_cols = [
-	"DER_mass_MMC",
-	"DER_mass_transverse_met_lep",
-	"DER_mass_vis",
-	"PRI_lep_phi",
-	"PRI_met_phi",
-	"PRI_tau_phi"
-]
-writeDone()
-print "	num rows:", traindata.shape[0]
-print
+		# PRI_jet_all_pt is purely zeroes in some groups where
+		# the other jet columns are all -999 (nan). In that case
+		# this has zero information to give us, and will cause
+		# headaches with the Delaunay Triangulation, so just
+		# remove it.
+		# 
+		# Update: similar logic for DER_pt_tot
+		if col_flags_str == "10000001111111110111110001110" or col_flags_str == "10000001111111110111110001111":
+			self.available_cols.remove("PRI_jet_all_pt")
+			self.available_cols.remove("DER_pt_tot")
 
-comparator_lookup = {}
-traindata["row_col_flags"] = calc_col_flags(traindata)
-for col_flags, group in traindata.groupby("row_col_flags"):
-	write("building VoronoiKdeComparator for {0:b}".format(col_flags))
-	comparator_lookup[col_flags] = comparator = VoronoiKdeComparator(
-		"{0:b}".format(col_flags),
-		group,
-		get_flagged_cols(col_flags, available_cols=target_cols)
-		# get_flagged_cols(col_flags)
-	)
-	writeDone()
-	to_pct = 100.0 / (comparator.num_s + comparator.num_b)
-	print "	[s: %02f%%, b: %02f%%]" % (comparator.num_s * to_pct, comparator.num_b * to_pct)
-print
+		if col_flags_str == "10001111111111110111110001110" or col_flags_str == "10001111111111110111110001111":
+			self.available_cols.remove("PRI_jet_all_pt")
 
-def score_df(df):
-	df["row_col_flags"] = calc_col_flags(df)
-	df["Class"] = ["MONKEY"] * df.shape[0]
-	df["confidence"] = ["MONKEY"] * df.shape[0]
-	for col_flags, group in df.groupby("row_col_flags"):
-		if col_flags in comparator_lookup:
-			comparator = comparator_lookup[col_flags]
-			class_, confidence = comparator.classify(group)
-			df["Class"][group.index] = class_
-			df["confidence"][group.index] = confidence
-	df = df.sort("confidence")
-	df["RankOrder"] = range(1, df.shape[0] + 1)
-	df = df.sort("EventId")
-	return df
+		self.comparator_set = []
+		for col_pair in choose(self.available_cols, 2):
+			self.comparator_set.append(
+				VoronoiKdeComparator(
+				"{0:s} ({1:s}, {2:s})".format(col_flags_str, col_pair[0], col_pair[1]),
+				dataframe,
+				col_pair
+				)
+			)
+		# self.comparator_set = [
+		# 	VoronoiKdeComparator(
+		# 		"foo", # "{0:b}".format(col_flags),
+		# 		dataframe,
+		# 		col_pair
+		# 	)
+		# 	for col_pair in choose(feature_cols, 2)
+		# ]
 
-write("classifying training rows")
-traindata = score_df(traindata)
-writeDone()
-
-print "AMS:", ams(traindata["Class"], traindata)
-print "% signal (actual):", (float(np.sum(traindata["Label"]=="s")) / np.sum(traindata.shape[0]))
-print "% signal (predicted):", (float(np.sum(traindata["Class"]=="s")) / np.sum(traindata.shape[0]))
-
-# write("loading test data")
-# testdata = loadTestData(TEST_LIMIT)
-# writeDone()
-
-# write("classifying test data")
-# testdata = score_df(testdata)
-# writeDone()
-
-# write("writing output")
-# testdata[["EventId", "RankOrder", "Class"]].to_csv(CSV_OUTPUT_FILE, header=True, index=False)
-# zf = zipfile.ZipFile(ZIP_OUTPUT_FILE, "w", zipfile.ZIP_DEFLATED)
-# zf.write(CSV_OUTPUT_FILE)
-# zf.close()
-# writeDone()
-
-writeDone(time.time() - global_start)
+	def classify(self, dataframe):
+		score_ratios = np.ones(dataframe.shape[0])
+		confidences = np.ones(dataframe.shape[0])
+		# print_df = pd.DataFrame()
+		for comparator in self.comparator_set:
+			sub_score_ratios = comparator.classify(dataframe)
+			# print_df[comparator.name] = sub_score_ratios
+			score_ratios = score_ratios * sub_score_ratios
+		# print print_df
+		# exit()
+		lookup = np.array(["b", "s"])
+		return (
+			lookup[(score_ratios > 1.0).astype(np.int)],
+			confidences
+		)
