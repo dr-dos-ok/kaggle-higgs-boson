1be55582ebb05a244d707056bfff4fca56d0a2b2
Brian Putnam <brian@brian-putnam.com>
2014-09-09 10:35:09 -0600

    added changeable cutoff value

--- neuralnet_classifier.py
+++ neuralnet_classifier.py
@@ -6,10 +6,11 @@ import numpy as np
 class ZNetClassifier(object):
 	def __init__(self, znet):
 		self.znet = znet
+		self.cutoff = 0.0
 
 	def classify(self, dataframe):
 		score = self.znet.forward(dataframe)
 		options = np.array(["b", "s"])
-		classes = options[(score[:,0] > score[:,1]).astype(np.int).ravel()]
+		classes = options[(score[:,0] - score[:,1] > self.cutoff).astype(np.int).ravel()]
 		confidence = np.max(score, axis=1) ** 2
 		return (classes, confidence)
