15ba4adb469a1fdfd3d5d4b56efd68088cb1d749
Brian Putnam <brian@brian-putnam.com>
2014-09-09 10:42:41 -0600

    cleanup, minor tweaks

--- neuralnet_train.py
+++ neuralnet_train.py
@@ -2,7 +2,6 @@ import numpy as np
 import neuralnet as nn
 import matplotlib.pyplot as plt
 
-from neuralnet_classifier import ZNetClassifier
 from bkputils import *
 
 import sys, bkputils, time
@@ -13,24 +12,15 @@ MIN_EPOCHS = 10
 # BATCH_SIZE = 100
 # VELOCITY_DECAY = 0.9
 # LEARNING_RATE = 0.001
+WEIGHT_DECAY = 0.05
 
-def train(net, alldata, learning_rate=0.01, velocity_decay=0.9, batch_size=100, validation_pct=0.2):
-	
-	validation_size = int(alldata.shape[0] * validation_pct)
-	index_shuffled = np.random.choice(alldata.index, alldata.shape[0])
-
-	validation_indices = index_shuffled[:validation_size]
-	training_indices = index_shuffled[validation_size:]
-
-	validation_set = alldata.ix[validation_indices]
-	training_set = alldata.ix[training_indices]
+def train(net, validation_set, training_set, learning_rate=0.01, velocity_decay=0.9, batch_size=100):
 
 	batch_cutoffs = np.concatenate([
 		np.arange(0, training_set.shape[0], batch_size),
 		np.array([training_set.shape[0]])
 	])
-
-	classifier = ZNetClassifier(net)
+	batches_per_epoch = batch_cutoffs.shape[0] - 1
 
 	train_err = get_err(net, training_set)
 	validation_err = get_err(net, validation_set)
@@ -44,6 +34,9 @@ def train(net, alldata, learning_rate=0.01, velocity_decay=0.9, batch_size=100, 
 	best_epoch = 0
 	velocity = net.zeros_like_flattened_weights()
 
+	# weight_mask = net.flattened_weight_mask(nn.BIASES)
+	weight_decay_mask = WEIGHT_DECAY # * weight_mask
+
 	overtrained = False
 	num_epochs = 0
 	bkputils.capture_sigint()
@@ -55,15 +48,12 @@ def train(net, alldata, learning_rate=0.01, velocity_decay=0.9, batch_size=100, 
 
 		shuffled_indices = training_set.index.values.copy()
 		np.random.shuffle(shuffled_indices)
-		shuffled_training_set = training_set.ix[shuffled_indices]
+		shuffled_training_set = training_set.loc[shuffled_indices]
 
 		#train epoch in batches of BATCH_SIZE
-		getbatch = 0.0
-		computegrad = 0.0
-		weight_updates = 0.0
-		set_weights = 0.0
-		# print
-		for batch_start, batch_stop in nn.adjacent_pairs(batch_cutoffs):
+		weights -= weights * weight_decay_mask
+		velocity = net.zeros_like_flattened_weights()
+		for batch_num, (batch_start, batch_stop) in enumerate(nn.adjacent_pairs(batch_cutoffs)):
 
 			batch = shuffled_training_set.iloc[batch_start:batch_stop]
 
@@ -75,12 +65,6 @@ def train(net, alldata, learning_rate=0.01, velocity_decay=0.9, batch_size=100, 
 			velocity *= velocity_decay
 			velocity += -(gradient * learning_rate)
 			weights += velocity
-
-			# num_epochs += 1
-			# train_err = get_err(net, training_set)
-			# validation_err = get_err(net, validation_set)
-			# print "train_err, validation_err:", train_err, validation_err
-			# plotter.plot(num_epochs, train_err, validation_err)
 
 		#epoch complete: evaluate error
 		num_epochs += 1
@@ -111,7 +95,7 @@ def train(net, alldata, learning_rate=0.01, velocity_decay=0.9, batch_size=100, 
 	return best_error
 
 def get_err(net, df):
-	errs, __ = net.err_fn(
+	errs = net.err_fn(
 		net.forward(df),
 		df[["train_s", "train_b"]].values
 	)
@@ -123,8 +107,15 @@ class Plotter(object):
 		self.train_errs = [0.0]
 		self.validation_errs = [0.0]
 
+		self.intermediate_x = [0.0]
+		self.intermediate_errs = [0.0]
+
 		plt.ion()
-		self.train_line, self.validation_line = plt.plot(self.x_vals, self.train_errs, "r-", self.x_vals, self.validation_errs, "b-")
+		self.train_line, self.validation_line, self.intermediate_line = plt.plot(
+			self.x_vals, self.train_errs, "r-",
+			self.x_vals, self.validation_errs, "b-",
+			self.intermediate_x, self.intermediate_errs, "m."
+		)
 		plt.show()
 
 	def plot(self, x, train_err, validation_err):
@@ -132,11 +123,23 @@ class Plotter(object):
 		self.train_errs.append(train_err)
 		self.validation_errs.append(validation_err)
 
+		self._plot()
+
+	def plot_intermediate(self, x, err):
+		self.intermediate_x.append(x)
+		self.intermediate_errs.append(err)
+
+		self._plot()
+
+	def _plot(self):
 		self.train_line.set_xdata(self.x_vals)
 		self.train_line.set_ydata(self.train_errs)
 
 		self.validation_line.set_xdata(self.x_vals)
 		self.validation_line.set_ydata(self.validation_errs)
+
+		self.intermediate_line.set_xdata(self.intermediate_x)
+		self.intermediate_line.set_ydata(self.intermediate_errs)
 
 		plt.axes().relim()
 		plt.axes().autoscale_view()
